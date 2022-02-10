6c8eeec95763803a0a9806bff185880ac22ebaa8
giuseppe romano <giuseppenadir.romano@gmail.com>
2022-02-10 16:56:34 +0100

    add a darkmode component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FooterComponent } from './layout/footer/footer.component';
+import { DarkmodeComponent } from './layout/elements/darkmode/darkmode.component';
 
 @NgModule({
-  declarations: [AppComponent, FooterComponent],
+  declarations: [AppComponent, FooterComponent, DarkmodeComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/layout/elements/darkmode/darkmode.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pancake-darkmode',
+  templateUrl: './darkmode.component.html',
+  styleUrls: ['./darkmode.component.css']
+})
+export class DarkmodeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
