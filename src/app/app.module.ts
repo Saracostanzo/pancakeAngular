eebbc9de32b18d4ed05a122b320ce8588a10b476
giuseppe romano <giuseppenadir.romano@gmail.com>
2022-02-10 17:10:59 +0100

    create earn scaff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './layout/footer/footer.component';
 import { DarkmodeComponent } from './layout/elements/darkmode/darkmode.component';
+import { EarnComponent } from './pages/earn/earn.component';
 
 @NgModule({
-  declarations: [AppComponent, FooterComponent, DarkmodeComponent],
+  declarations: [AppComponent, FooterComponent, DarkmodeComponent, EarnComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/pages/earn/earn.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pancake-earn',
+  templateUrl: './earn.component.html',
+  styleUrls: ['./earn.component.css']
+})
+export class EarnComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
