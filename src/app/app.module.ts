8b1fbd8363caff52330d96f01fdfbb5dace3a8d3
giuseppe romano <giuseppenadir.romano@gmail.com>
2022-02-10 18:06:08 +0100

    add earn header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './layout/footer/footer.component';
 import { DarkmodeComponent } from './layout/elements/darkmode/darkmode.component';
 import { EarnComponent } from './pages/earn/earn.component';
+import { EarnHeaderComponent } from './pages/earn/earn-header/earn-header.component';
 
 @NgModule({
-  declarations: [AppComponent, FooterComponent, DarkmodeComponent, EarnComponent],
+  declarations: [AppComponent, FooterComponent, DarkmodeComponent, EarnComponent, EarnHeaderComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/pages/earn/earn-header/earn-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pancake-earn-header',
+  templateUrl: './earn-header.component.html',
+  styleUrls: ['./earn-header.component.css']
+})
+export class EarnHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
