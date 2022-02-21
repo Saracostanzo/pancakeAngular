7389bf06adf253dd3dd2c18b0eb2bf693489588b
Sara <costanzosara96@gmail.com>
2022-02-21 15:50:04 +0100

    fine navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './layout/footer/footer.component';
 import { DarkmodeComponent } from './layout/elements/darkmode/darkmode.component';
 import { EarnComponent } from './pages/earn/earn.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent, FooterComponent, DarkmodeComponent, EarnComponent],
+  declarations: [AppComponent, FooterComponent, DarkmodeComponent, EarnComponent, NavbarComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
