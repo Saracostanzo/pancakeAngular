1a99a4a5c2f0f644bc408a7746779d1a590b8960
Sara <costanzosara96@gmail.com>
2022-02-22 12:03:44 +0100

    primo commit homepage html e css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { FooterComponent } from './layout/footer/footer.component';
 import { DarkmodeComponent } from './layout/elements/darkmode/darkmode.component';
 import { EarnComponent } from './pages/earn/earn.component';
 import { NavbarComponent } from './layout/navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, FooterComponent, DarkmodeComponent, EarnComponent, NavbarComponent],
+  declarations: [AppComponent, FooterComponent, DarkmodeComponent, EarnComponent, NavbarComponent, HomeComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/pages/home/interfacce home/interfaccia-sections-buttons.ts
@@ -0,0 +1,10 @@
+export interface SectionButton {
+  h1?: string;
+  span?:string;
+  h2?: string;
+  h3?:string;
+  h4?: string;
+  h5?:string;
+  buttons?: string;
+  a?:string;
+  }
