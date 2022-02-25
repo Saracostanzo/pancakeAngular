ee464ada07a0f086afdb886aa0cf032fcd1e7cdc
Sara <costanzosara96@gmail.com>
2022-02-25 16:48:21 +0100

    inizio info

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ChartModule } from 'angular-highcharts';
 
 import { AppComponent } from './app.component';
 import { FooterComponent } from './layout/footer/footer.component';
 import { DarkmodeComponent } from './layout/elements/darkmode/darkmode.component';
 import { EarnComponent } from './pages/earn/earn.component';
 import { NavbarComponent } from './layout/navbar/navbar.component';
+import { InfoComponent } from './pages/info/info.component';
+import { GraficoComponent } from './grafico/grafico.component';
 
 @NgModule({
-  declarations: [AppComponent, FooterComponent, DarkmodeComponent, EarnComponent, NavbarComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, FooterComponent, DarkmodeComponent, EarnComponent, NavbarComponent, InfoComponent, GraficoComponent],
+  imports: [BrowserModule , ChartModule ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/pages/info/grafico/grafico.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import * as Highcharts from 'highcharts';
+
+@Component({
+  selector: 'pancake-grafico',
+  templateUrl: './grafico.component.html',
+  styleUrls: ['./grafico.component.css']
+})
+export class GraficoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
