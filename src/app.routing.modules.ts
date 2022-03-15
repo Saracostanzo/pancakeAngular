1c9ade5b1ec3b89e4f32b14b88ae9d024f166885
Sara <costanzosara96@gmail.com>
2022-03-15 10:14:43 +0100

    inizio routing

---
+++ src/app.routing.modules.ts
@@ -0,0 +1,14 @@
+// import { NgModule } from "@angular/core";
+// import{Routes , RouterModule}from "@angular/router";
+
+// const routers: Routes=[
+//   {path:"home", component: HomeComponent },
+//   { path:"projects", component: ProjectContainerComponent}
+// ];
+// @NgModule({
+// imports:[
+//   RouterModule.forRoot(routers)
+// ],
+// exports:[RouterModule]
+// })
+//  export class AppRoutingModule{}
